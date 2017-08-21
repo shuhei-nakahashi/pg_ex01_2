fd64e84d5074fa412a579ba620039b30cc61142c
shuhei <shuhei@192.168.11.25>
2017-08-22 01:34:03 +0900

    初回のコミット

---
+++ src/kadai2/RomanNumberHenkan.java
@@ -0,0 +1,57 @@
+package kadai2;
+
+public class RomanNumberHenkan {
+
+	//1～20の数をローマ数字に変換するメソッド
+	public String romanNumberHenkan(int input) {
+
+		int futaketameNum = input/10;//2桁目の数
+		int hitoketameNum = input%10;//1桁目の数
+
+		return romanNumKurikaeshi(futaketameNum,'X') + hitoketameToRomanNum(hitoketameNum);
+	}
+
+	//1桁目の数を入力としローマ数字に変換するメソッド
+	private String hitoketameToRomanNum(int inputHitoketameNum) {
+
+		int SyouDevidedByFive = inputHitoketameNum / 5; //入力を5で割った商
+		int amariDevidedByFive = inputHitoketameNum % 5;//入力を5で割った余り
+
+		//入力が9であればIXを返す
+		if(inputHitoketameNum == 9)
+			return "IX";
+		//入力が4であればIVを返す
+		else if(inputHitoketameNum == 4)
+			return "IV";
+		//入力が5であればVを返す
+		else if(inputHitoketameNum == 5)
+			return "V";
+		//入力が6～8であればをローマ数字に変換して返す
+		else if(sixToEightCheck(inputHitoketameNum)){
+			return "V"+ romanNumKurikaeshi(amariDevidedByFive,'I');
+		}
+		//入力が上記以外(1～3)であればをローマ数字に変換して返す
+		else
+			return romanNumKurikaeshi(amariDevidedByFive,'I');
+
+	}
+
+	//入力が6～8か判定するメソッド
+	private boolean sixToEightCheck(int inputNum) {
+		return inputNum/5 == 1;
+	}
+
+	//任意のローマ文字を繰り返し付加するメソッド
+	private String romanNumKurikaeshi(int kurikaeshi, char romanMoji) {
+
+		//出力を初期化
+		String kurikaeshiOutput = "";
+
+		//任意のローマ文字を繰り返し付加
+		for (int i=0; i<kurikaeshi; i++){
+			kurikaeshiOutput = kurikaeshiOutput + romanMoji;
+		}
+		return kurikaeshiOutput;
+	}
+
+}
---
+++ src/kadai2/RomanNumberTest.java
@@ -0,0 +1,56 @@
+package kadai2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RomanNumberTest {
+
+	@Test
+	public void testIncludingNineOrFour() {
+		RomanNumberHenkan rNH = new RomanNumberHenkan();
+		assertEquals("XIX",rNH.romanNumberHenkan(19));
+		assertEquals("XIV",rNH.romanNumberHenkan(14));
+		assertEquals("IX",rNH.romanNumberHenkan(9));
+		assertEquals("IV",rNH.romanNumberHenkan(4));
+	}
+
+	@Test
+	public void testFiveNoBaisu() {
+		RomanNumberHenkan rNH = new RomanNumberHenkan();
+		assertEquals("XX",rNH.romanNumberHenkan(20));
+		assertEquals("XV",rNH.romanNumberHenkan(15));
+		assertEquals("X",rNH.romanNumberHenkan(10));
+		assertEquals("V",rNH.romanNumberHenkan(5));
+	}
+
+	@Test
+	public void testSixToEight() {
+		RomanNumberHenkan rNH = new RomanNumberHenkan();
+		assertEquals("VIII",rNH.romanNumberHenkan(8));
+		assertEquals("VII",rNH.romanNumberHenkan(7));
+		assertEquals("VI",rNH.romanNumberHenkan(6));
+	}
+
+	@Test
+	public void testSixteenToEighteen() {
+		RomanNumberHenkan rNH = new RomanNumberHenkan();
+		assertEquals("XVIII",rNH.romanNumberHenkan(18));
+		assertEquals("XVII",rNH.romanNumberHenkan(17));
+		assertEquals("XVI",rNH.romanNumberHenkan(16));
+	}
+
+	@Test
+	public void testOneToThree() {
+		RomanNumberHenkan rNH = new RomanNumberHenkan();
+		assertEquals("III",rNH.romanNumberHenkan(3));
+		assertEquals("II",rNH.romanNumberHenkan(2));
+		assertEquals("I",rNH.romanNumberHenkan(1));
+		assertEquals("XIII",rNH.romanNumberHenkan(13));
+		assertEquals("XII",rNH.romanNumberHenkan(12));
+		assertEquals("XI",rNH.romanNumberHenkan(11));
+	}
+
+
+
+}
